5b49317a559dcdad046924473ed9c921a07b2686
Alexander Santos <lekxandy@hotmail.com>
2021-05-23 12:36:59 -0300

    created 2 endpoints, add produto to insert into database and findall to list all products in database

--- src/main/java/com/alexander/produtos/controller/ProdutoController.java
+++ src/main/java/com/alexander/produtos/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.alexander.produtos.controller;
 import com.alexander.produtos.model.ProdutoModel;
 import com.alexander.produtos.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,6 +28,19 @@ public class ProdutoController {
     @RequestMapping(value = "/contato", method = RequestMethod.GET)
     public @ResponseBody String contato(){
         return "Contato";
+    }
+
+    @RequestMapping(value = "/produtos", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.FOUND)
+    public @ResponseBody List<ProdutoModel> listAll(){
+
+        return (List<ProdutoModel>) produtoRepository.findAll();
+    }
+
+    @RequestMapping(value = "/produtos", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody ProdutoModel addProduto(@RequestBody ProdutoModel produto){
+        return produtoRepository.save(produto);
     }
 
 }
